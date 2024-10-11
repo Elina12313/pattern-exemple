537001b7a300e8236fe37ccb2df8062374748013
ekbalina <lina_balina@mail.ru>
2024-10-11 10:04:09 +0300

    122

--- src/Factory/MyShapeCreator.java
+++ src/Factory/MyShapeCreator.java
@@ -1,4 +1,13 @@
 package Factory;
 
 public class MyShapeCreator {
+    private ShapeMaker shapeMaker;
+
+    public MyShapeCreator(ShapeMaker shapeMaker) {
+        this.shapeMaker = shapeMaker;
+    }
+    public Object createShape() {
+        return shapeMaker.buildShape();
+    }
+
 }
